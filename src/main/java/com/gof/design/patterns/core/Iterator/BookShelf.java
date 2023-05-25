cf472b0d1844bf22e53451dcb0d9a399d604db41
toranoko114 <toranoko114.business@gmail.com>
2023-05-25 21:34:21 +0900

    Iterator練習問題_配列をリストに変更

--- src/main/java/com/gof/design/patterns/core/Iterator/BookShelf.java
+++ src/main/java/com/gof/design/patterns/core/Iterator/BookShelf.java
@@ -1,30 +1,30 @@
 package com.gof.design.patterns.core.Iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * 本棚を表すクラス
  */
 public class BookShelf implements Iterable<Book> {
 
-  private final Book[] books;
-  private int last = 0;
+  private final List<Book> books;
 
-  public BookShelf(int maxsize) {
-    this.books = new Book[maxsize];
+  public BookShelf(int initSize) {
+    this.books = new ArrayList<>(initSize);
   }
 
   public Book getBookAt(int index) {
-    return books[index];
+    return books.get(index);
   }
 
   public void appendBook(Book book) {
-    this.books[last] = book;
-    last++;
+    this.books.add(book);
   }
 
   public int getLength() {
-    return last;
+    return books.size();
   }
 
   @Override
