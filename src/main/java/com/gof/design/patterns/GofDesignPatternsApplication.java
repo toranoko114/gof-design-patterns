541fb3d1375b0658d41679f615e5d8e6902ddbad
toranoko114 <toranoko114.business@gmail.com>
2023-05-25 21:22:58 +0900

    first commit

---
+++ src/main/java/com/gof/design/patterns/GofDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.gof.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gof/design/patterns/GofDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gof.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
